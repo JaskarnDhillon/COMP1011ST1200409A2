8fdb70ae2c0b893e603647af9109973e4c5a9944
JaskarnDhillon <mail.jaskarndhillon@gmail.com>
2023-11-24 15:24:37 -0500

    Removed unnecessary files, added gson maven dependency, and added gson imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.comp1011st1200409a2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
 
 
-    opens com.example.comp1011st1200409a2 to javafx.fxml;
+
+    opens com.example.comp1011st1200409a2 to javafx.fxml, com.google.gson;
     exports com.example.comp1011st1200409a2;
 }
